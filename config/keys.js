d6773f0c345c431c7fed32d0a14b573e35b7c150
johcare <johcare2@gmail.com>
2017-10-14 16:55:35 -0400

    fr

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+
+  module.exports = {
+      googleClientID: "964806699295-831rku0eet3jct59i0f7ju8s85g9u5k3.apps.googleusercontent.com",
+      googleClientSecret: "sfkEeHhyQY7Mge8qz3OXJUvu",
+      mongoURI: 'mongodb://zorro:12345@ds133281.mlab.com:33281/elka-pelka',
+      cookieKey:'wrtghtgrfedwefgrhtyjhgfdsfgrthjytgf'
+};
+    
